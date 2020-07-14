265306efe73ec03f3721b72aa24a860096607639
semen-ksv <ksvsem@gmail.com>
2020-07-14 18:02:10 +0300

    1--add Post model

--- Test_task_Django_REST/settings.py
+++ Test_task_Django_REST/settings.py
@@ -27,6 +27,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+INTERNAL_IPS = ['127.0.0.1', ]
 
 # Application definition
 
@@ -37,7 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'debug_toolbar',
     'rest_framework',
+    'post_api',
 ]
 
 MIDDLEWARE = [
@@ -48,6 +51,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
 ]
 
 ROOT_URLCONF = 'Test_task_Django_REST.urls'
--- post_api/models.py
+++ post_api/models.py
@@ -1,3 +1,32 @@
-from django.db import models
+from time import time
 
-# Create your models here.
+from django.contrib.auth.models import User
+from django.db import models
+from django.utils import timezone
+from django.utils.text import slugify
+
+
+class Post(models.Model):
+    """Model for posts at site"""
+
+    title = models.CharField(max_length=150, db_index=True)
+    slug = models.SlugField(max_length=150, blank=True, unique=True)
+    content = models.TextField(blank=True)
+    date_posted = models.DateField(default=timezone.now)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    like = models.IntegerField(blank=True, default=0)
+
+    def __str__(self):
+        return self.title
+
+    @staticmethod
+    def generate_slug(title):
+        """generate new slug for new title"""
+        new_slug = slugify(title, allow_unicode=True)
+        return new_slug + '_' + str(int(time()))
+
+    def save(self, *args, **kwargs):
+        """save new generated slug"""
+        if not self.id:
+            self.slug = self.generate_slug(self.title)
+        super().save(*args, **kwargs)
