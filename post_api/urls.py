ad5d57e1668311b9632d7180ce04e547ff2348dc
semen-ksv <ksvsem@gmail.com>
2020-07-14 22:01:05 +0300

    4--add view for deleting Post

--- post_api/urls.py
+++ post_api/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import PostView, PostDetailView, PostCreateView, PostUpdateView
+from .views import PostView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView
 
 urlpatterns = [
     path('', PostView.as_view()),
     path('create/', PostCreateView.as_view()),
-    path('<slug>/', PostDetailView.as_view()),
-    path('<slug>/update', PostUpdateView.as_view()),
+    path('<str:slug>/', PostDetailView.as_view()),
+    path('<str:slug>/update', PostUpdateView.as_view()),
+    path('<str:slug>/delete', PostDeleteView.as_view()),
 
 ]
--- post_api/views.py
+++ post_api/views.py
@@ -49,6 +49,7 @@ class PostUpdateView(APIView):
     permission_classes = (IsAuthenticated,)
 
     def put(self, request, slug):
+        # Get post with this slug
         saved_post = get_object_or_404(Post.objects.all(), slug=slug)
         updated_post_data = request.data
         serializer = PostUpdateSerializer(instance=saved_post, data=updated_post_data, partial=True)
@@ -59,3 +60,17 @@ class PostUpdateView(APIView):
             })
         else:
             return Response(status=404)
+
+
+class PostDeleteView(APIView):
+    """Update single post for authenticated users"""
+    authentication_classes = (BasicAuthentication,)
+    permission_classes = (IsAuthenticated,)
+
+    def delete(self, request, slug):
+        # Get post with this slug
+        article = get_object_or_404(Post.objects.all(), slug=slug)
+        article.delete()
+        return Response({
+            "message": f"Article with id `{article.title}` has been deleted."
+        }, status=204)
